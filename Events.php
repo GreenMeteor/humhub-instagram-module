b86c3e912cc6a7c89a194963cedfb7b2e45ed39c
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-10-06 23:49:30 -0700

    Chg: ::className() is deprecated + PHP 7.2 support

--- Events.php
+++ Events.php
@@ -6,7 +6,7 @@ use yii\helpers\Url;
 use humhub\modules\instagram\widgets\InstagramFrame;
 use humhub\models\Setting;
 
-class Events extends \yii\base\Object
+class Events extends \yii\base\BaseObject
 {
 
     public static function onAdminMenuInit(\yii\base\Event $event)
@@ -26,8 +26,8 @@ public static function addInstagramFrame($event)
         if (Yii::$app->user->isGuest) {
             return;
         }
-        $event->sender->view->registerAssetBundle(Assets::className());
-        $event->sender->addWidget(InstagramFrame::className(), [], [
+        $event->sender->view->registerAssetBundle(Assets::class);
+        $event->sender->addWidget(InstagramFrame::class, [], [
             'sortOrder' => Setting::Get('timeout', 'instagram')
         ]);
     }
